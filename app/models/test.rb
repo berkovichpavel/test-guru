66894d506117ef0d6ad2751b378baa2893a576f2
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-11-20 00:06:31 +0300

    Add third task

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.all_titles_by_category(name)
+    joins(:category)
+        .where(categories: { title: name })
+        .order(title: :desc)
+        .pluck(:title)
+  end
 end
