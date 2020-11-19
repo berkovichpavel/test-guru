9f4cc6e42a10d44521c291e062b039d035b2c3b7
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-11-20 00:42:56 +0300

    Solve problems

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
 
   def self.all_titles_by_category(name)
     category_id = Category.where(title: name).first.id
-    self.where(category_id: category_id).order(title: :desc).pluck(:title)
+    where(category_id: category_id).order(title: :desc).pluck(:title)
   end
 end
