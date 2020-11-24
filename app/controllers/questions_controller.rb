014ac103bcd0a9006930c4c064f97c608fe2d911
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-11-24 10:54:28 +0300

    Add second  step

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,43 @@
 class QuestionsController < ApplicationController
-  class QuestionssController < ApplicationController
 
-    before_action :find_test, only: %i[index create new]
-    before_action :find_question, only: %i[show]
-
-
-    def index
-      @fish = 'fish'
-      @test = Test.find(params[:test_id])
-      @questions = Question.where(test: @test)
-    end
-
-    def show; end
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy]
 
 
-    private
-
-    def find_question
-      @question = Question.find(params[:id])
-    end
-
-    def find_test
-      @test = Test.find(params[:test_id])
-    end
-
-    def rescue_with_question_not_found
-      render plain: "Question doesn't exist"
-    end
-
+  def index
+    @test = Test.find(params[:test_id])
+    @questions = Question.where(test: @test)
   end
+
+  def show; end
+
+  def new; end
+
+  def create
+    question = @test.questions.create!(question_params)
+    redirect_to question_path(question)
+  end
+
+  def destroy
+    @question.destroy!
+  end
+
+  private
+
+  def question_params
+    params.permit(:body)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Question doesn't exist" 
+  end
+
 end
